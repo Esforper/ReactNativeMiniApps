13b964410224f89ffef467eeb6b18312f521b643
Esforper <dolanbayomerfaruk@gmail.com>
2024-12-06 23:57:22 +0300

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+
+// Sayfaları import edin
+import Page1 from './screens/Page1';
+import Page2 from './screens/Page2';
+import Page3 from './screens/Page3';
+import Page4 from './screens/Page4';
+
+// Drawer Navigator oluşturun
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Sayfa 1">
+        <Drawer.Screen name="Sayfa 1" component={Page1} />
+        <Drawer.Screen name="Sayfa 2" component={Page2} />
+        <Drawer.Screen name="Sayfa 3" component={Page3} />
+        <Drawer.Screen name="Sayfa 4" component={Page4} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
